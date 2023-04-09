656add471067fd08fa151ab06b6a476f8e5bdfbc
Humayun Kabir <humayunkabir6491@gmail.com>
2023-04-09 17:23:40 +0600

    folder add

--- scroll-to-top-plugin.php
+++ scroll-to-top-plugin.php
@@ -11,14 +11,14 @@ License: GPL2
 
 // Add the required scripts and styles
 function add_scroll_to_top_scripts() {
-    wp_enqueue_style( 'scroll-to-top-style', plugin_dir_url( __FILE__ ) . 'scroll-to-top.css' );
-    wp_enqueue_script( 'scroll-to-top-script', plugin_dir_url( __FILE__ ) . 'scroll-to-top.js', array('jquery'), '1.0', true );
+    wp_enqueue_style( 'scroll-to-top-style', plugin_dir_url( __FILE__ ) . 'css/scroll-to-top.css' );
+    wp_enqueue_script( 'scroll-to-top-script', plugin_dir_url( __FILE__ ) . 'js/scroll-to-top.js', array('jquery'), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'add_scroll_to_top_scripts' );
 
 // Add the "Scroll to Top" button
 function scroll_to_top_button() {
-    echo '<a href="#" id="scroll-to-top"><i class="fa fa-angle-up"></i></a>';
+    echo '<a href="#" class="scroll-to-top"><i class="fa fa-arrow-up" aria-hidden="true"></i></a>';
 }
 add_action( 'wp_footer', 'scroll_to_top_button' );
 
